e83fe54acb760ea0bab876f9025f99dbbdf5f6e0
VG <vladyslav.gnatiuk@gmail.com>
2025-01-15 23:00:33 -0500

    cleanup and start working on nginx

--- load-test/load_test.sh
+++ load-test/load_test.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
-set -e
 
-for c in 10 25 50 100
-do
-    siege -c$c -t10S -v -H "Content-Type: application/json" -f /tests/urls.txt
-done
+# Test image URL
+IMAGE_URL="http://example.com/test.jpg"
+
+# Make first request
+echo "First request:"
+curl -I $IMAGE_URL
+
+echo "\nSecond request:"
+curl -I $IMAGE_URL
+
+echo "\nThird request:"
+curl -I $IMAGE_URL
+
+# Test cache purge
+echo "\nPurging cache:"
+curl -X PURGE http://example.com/purge/test.jpg
