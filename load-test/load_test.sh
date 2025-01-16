f94fddb45f9b859dd4f3196484012a3adb5b2df2
VG <vladyslav.gnatiuk@gmail.com>
2025-01-16 00:01:51 -0500

    cahce header exists but not working

--- load-test/load_test.sh
+++ load-test/load_test.sh
@@ -1,18 +1,25 @@
 #!/bin/bash
 
-# Test image URL
-IMAGE_URL="http://example.com/test.jpg"
+# Test image URL (using localhost since we're using host network)
+IMAGE_URL="http://localhost/images/img1.jpg"
+PURGE_URL="http://localhost/purge/images/img1.jpg"
 
 # Make first request
-echo "First request:"
-curl -I $IMAGE_URL
+echo "First request (should be MISS):"
+curl -I "$IMAGE_URL"
 
-echo "\nSecond request:"
-curl -I $IMAGE_URL
+echo -e "\nSecond request (should be MISS):"
+curl -I "$IMAGE_URL"
 
-echo "\nThird request:"
-curl -I $IMAGE_URL
+echo -e "\nThird request (should be HIT):"
+curl -I "$IMAGE_URL"
+
+echo -e "\nThird request (should be HIT):"
+curl -I "$IMAGE_URL"
 
 # Test cache purge
-echo "\nPurging cache:"
-curl -X PURGE http://example.com/purge/test.jpg
+echo -e "\nPurging cache:"
+curl -X PURGE "${PURGE_URL//[[:space:]]/}"
+
+echo -e "\nFourth request (should be MISS after purge):"
+curl -I "$IMAGE_URL"
